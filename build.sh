6fcb4dc2735c15b55904df31384718276bdc6c06
pranav1698 <hulkcodexl16@gmail.com>
2019-05-27 02:18:49 +0530

    Adding geniso package

--- build.sh
+++ build.sh
@@ -59,7 +59,7 @@ sudo mksquashfs chroot image/casper/filesystem.squashfs -noappend -no-progress
 # Creating the ISO image from the tree
 IMAGE_NAME=${IMAGE_NAME:-"CUSTOM ${release} $(date -u +%Y%m%d) - ${arch}"}
 ISOFILE=CUSTOM-${release}-$(date -u +%Y%m%d)-${arch}.iso
-
+sudo apt-get install genisoimage
 sudo genisoimage -r -V "$IMAGE_NAME" -cache-inodes -J -l \
   -b isolinux/isolinux.bin -c isolinux/boot.cat \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
